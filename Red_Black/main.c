4b6525a5234dee00cc79b70b9def0b12412f910a
juninhoojl <juninhopc@icloud.com>
2019-06-19 23:40:43 -0300

    Initial Commit

---
+++ Red_Black/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Red_Black
+//
+//  Created by José Luiz Junior on 19/06/19.
+//  Copyright © 2019 José Luiz Junior. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
